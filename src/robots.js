f80faf1c2dd04c8afa866f690914d13b8c73c6b6
Mike.Mueller <MichaelM.Dev@gmail.com>
2018-07-26 10:44:12 -0400

    Initial commit for robofriends app

---
+++ src/robots.js
@@ -0,0 +1,12 @@
+export const robots = [
+  {id: 1, name: 'James Hetfield', email: 'Metallica@emails.com'},
+  {id: 2, name: 'Lars Ulrich', email: 'downWithNapster@naptime.org'},
+  {id: 3, name: 'Dave Rapoza', email: 'eRopez@dopeboyz.gov'},
+  {id: 4, name: 'Dan Warren', email: 'WarinDan@malesOnly.love'},
+  {id: 5, name: 'Brennan Shaubbe', email: 'BeShabs@mails.com'},
+  {id: 6, name: 'Gooky McToots', email: 'Trump2020@boots.gov'},
+  {id: 7, name: 'Emma Watson', email: 'BootynBrains@mails.com'},
+  {id: 8, name: 'Shane Karson', email: 'Shane2Kar@mail.com'},
+  {id: 9, name: 'Violet Summers', email: 'Innit4money@hooks.com'},
+  {id: 10, name: 'Mitch Mitron', email: 'Mitchtron@mitts.com'}
+]
